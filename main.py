5d08557103045bc49366d296ccd895f07d329662
Mandarin <zurual@mail.ru>
2023-07-31 12:40:46 +0300

    add cycle

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ else:  # если же было введено не число
     quit()  # выход из программы
 
 random_number = random.randint(0, top_of_range) # диапозон от 0 до числа которое названо
-print(random_number)
+
+
+while True:
+    user_guess = input('сделать предположение: ')
+    if user_guess.isdigit():  # Функция String isdigit() в Python возвращает True, если все символы в строке являются цифрами, в противном случае – False.
+        user_guess = int(user_guess)  # переводим текст "число" в int("число"),
+                                      # чтобы было число
+    else:           # если же было введено не число
+        print('Введите цифру в следующий раз')
+        continue    # возврат к началу цикла
+
+    if user_guess == random_number:
+        print('Ты угадал!!!')
+        break       # если угадал то останавливаем цикл
+    else:
+        print('Ты не угадал...')
 
 
 
@@ -24,3 +39,4 @@ print(random_number)
 
 
 # https://www.youtube.com/watch?v=DLn3jOsNRVE&t=3020s
+# 33:10
