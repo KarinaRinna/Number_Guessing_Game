ff02c3740f23a34626a0dc535b3b497a73035509
Mandarin <zurual@mail.ru>
2023-07-31 12:54:18 +0300

    I added the hints.

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ else:  # если же было введено не число
     quit()  # выход из программы
 
 random_number = random.randint(0, top_of_range) # диапозон от 0 до числа которое названо
-
+guesses = 0         # попытки угадать число
 
 while True:
+    guesses += 1
     user_guess = input('сделать предположение: ')
     if user_guess.isdigit():  # Функция String isdigit() в Python возвращает True, если все символы в строке являются цифрами, в противном случае – False.
         user_guess = int(user_guess)  # переводим текст "число" в int("число"),
@@ -29,8 +30,13 @@ while True:
         print('Ты угадал!!!')
         break       # если угадал то останавливаем цикл
     else:
-        print('Ты не угадал...')
+        if user_guess > random_number:
+            print('Число меньше')
+        else:
+            print('Число больше')
 
+
+print('У тебя получилось, ты угадал с', guesses, 'раз')
 
 
 
